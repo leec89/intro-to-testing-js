ea224c861f76a9bcdff3e17f49286832ea1c8b3d
leec89 <leec89@gmail.com>
2022-04-11 16:45:00 -0500

    intro to testing - thru ex7 complete

--- code.js
+++ code.js
@@ -3,6 +3,6 @@ const helloWorld = function() {
     return "Hello, World!";
 };
 
-const sayHello = function() {
-    return ' ';
+const sayHello = function(input) {
+    return "Hello, " + input + "!";
 }
